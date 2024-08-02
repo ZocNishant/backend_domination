4599442b4abbc520b1b7b18855dcb21580cc53e6
Zoc Nishant <zocnishant@gmail.com>
2024-08-02 07:57:18 +0545

    test

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-let a = 12;
-let b = 13;
+const fs = require("fs");
 
-module.exports.a = a;
-module.exports.b = b;
+// fs.writeFile("test.txt", "K xa mero vaii.", function (err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Pasa sabai ramroo vo.");
+//   }
+// });
+
+// fs.readFile("test.txt", "utf-8", function (err, data) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(data);
+//   }
+// });
+
+fs.appendFile("test.txt", " k xa pasa aru.", function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
--- script2.js
+++
@@ -1,3 +0,0 @@
-const data = require("./script");
-
-console.log(data);
