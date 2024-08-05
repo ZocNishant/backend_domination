4ffa209c6784c74ee97eabfe824934f74b2965c7
Zoc Nishant <zocnishant@gmail.com>
2024-08-05 18:53:18 +0545

    a

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require("express");
+
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Pasa");
+let data = [1, 2, 3, 4, 5];
+
+app.get("/", (req, res, next) => {
+  try {
+    res.send("pasa");
+  } catch (err) {
+    next(err);
+  }
 });
 
-app.use(function (err, req, res, next) {
-  res.status(500).send(err.message);
+app.get("/data", (req, res) => {
+  res.send(data);
+});
+
+app.post("/data/:number", (req, res) => {
+  data.push(parseInt(req.params.number));
+  res.send(data);
 });
 
 app.listen(3000);
