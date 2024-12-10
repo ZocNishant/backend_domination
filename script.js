3110a2b647438b5b014d598272cb4d3450c331e3
Nishant Zoc <zocnishant@gmail.com>
2024-12-10 19:40:00 +0545

    basics

---
+++ script.js
@@ -0,0 +1,77 @@
+// variables
+
+var a = 12;
+
+let b = 13;
+
+// data types
+
+// primitives => other than brackets
+// reference => {}, [], ()
+
+//operators
+
+//function
+
+// function hello () {
+//     // function statements
+// }
+
+// function() {
+//     // anonymous function
+// }
+
+// const a = () => {
+//   // arrow function
+// };
+
+// callbacks
+
+// function connectToServer(movies) {
+//   console.log("Connecting to Server.");
+//   setTimeout(function () {
+//     console.log("Connected to server.");
+//     movies();
+//   }, 2000);
+// }
+
+// connectToServer(function () {
+//   console.log("Fetching Movies.");
+// });
+
+// Promises
+
+// function connectToServer() {
+//   return new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//       resolve("Conected.");
+//     }, 2000);
+//   });
+// }
+
+// connectToServer().then(function (response) {
+//   console.log(response);
+// });
+
+// for loop
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// foreach => array
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+// arr.forEach((ele) => {
+//   console.log(ele);
+// });
+
+let naam = {
+  age: 2,
+  isTrue: true,
+};
+
+for (var val in naam) {
+  console.log(naam[val]);
+}
