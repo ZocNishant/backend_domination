bd49661cfd19f45752e6caa3559d89f6124d500f
Nishant Zoc <zocnishant@gmail.com>
2024-12-22 19:08:36 +0545

    .

--- server.js
+++ server.js
@@ -1,35 +1,15 @@
-const exp = require("constants");
 const express = require("express");
-
-const expressSession = require("express-session");
-const flash = require("connect-flash");
-
-const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
-
-app.use(
-  expressSession({
-    resave: flash,
-    saveUninitialized: false,
-    secret: "pasa k xa",
-  })
-);
-
-app.use(flash());
-
 app.get("/", (req, res) => {
-  res.send("Hello! Welcome to the server.");
+  res.send(req.url);
 });
 
-app.get("/data", (req, res) => {
-  res.send("Done.");
+app.get("/profile/:username", (req, res) => {
+  res.send(req.params.username + " Hello");
 });
 
-app.listen(8888);
-
-// cookie is for saving data in browser
-// session is for saving data in server
-// comment
+app.listen(8888, () => {
+  console.log("Hello");
+});
